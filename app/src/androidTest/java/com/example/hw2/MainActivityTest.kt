5399802491e8dc15687a7c9e400c9b7c9a4dcf48
eeee905 <kaotzuchi0905@gmail.com>
2026-04-21 23:04:43 +0800

    fix

--- app/src/androidTest/java/com/example/hw2/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/hw2/MainActivityTest.kt
@@ -7,28 +7,22 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.junit.Rule
-=======
 import android.content.pm.ActivityInfo
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.runners.AndroidJUnit4
->>>>>>> 0534d5d6b4d2879e35beacc3d644c8db2250d97f
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-<<<<<<< HEAD
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun socialAccount_shouldNotExist() {
         onView(withText("kao_905")).check(doesNotExist())
-=======
     @Test
     fun image_is_not_displayed_in_landscape() {
         val scenario = ActivityScenario.launch(MainActivity::class.java)
@@ -38,6 +32,5 @@ class MainActivityTest {
         }
 
         onView(withId(R.id.profileImage)).check(doesNotExist())
->>>>>>> 0534d5d6b4d2879e35beacc3d644c8db2250d97f
     }
 }
