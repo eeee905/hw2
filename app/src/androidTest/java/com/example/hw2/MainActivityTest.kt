388bd50ba3f7e2b761b2007f174969ce24aac07e
eeee905 <kaotzuchi0905@gmail.com>
2026-04-21 21:09:32 +0800

    add icon

--- app/src/androidTest/java/com/example/hw2/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/hw2/MainActivityTest.kt
@@ -1,25 +1,22 @@
 package com.example.hw2
 
-import android.content.pm.ActivityInfo
-import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun image_is_not_displayed_in_landscape() {
-        val scenario = ActivityScenario.launch(MainActivity::class.java)
-
-        scenario.onActivity { activity ->
-            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-        }
-
-        onView(withId(R.id.profileImage)).check(doesNotExist())
+    fun socialAccount_shouldNotExist() {
+        onView(withText("kao_905")).check(doesNotExist())
     }
 }
