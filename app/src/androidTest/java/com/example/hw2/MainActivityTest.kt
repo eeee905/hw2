0534d5d6b4d2879e35beacc3d644c8db2250d97f
eeee905 <kaotzuchi0905@gmail.com>
2026-04-21 21:09:32 +0800

    first commit

---
+++ app/src/androidTest/java/com/example/hw2/MainActivityTest.kt
@@ -0,0 +1,25 @@
+package com.example.hw2
+
+import android.content.pm.ActivityInfo
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Test
+    fun image_is_not_displayed_in_landscape() {
+        val scenario = ActivityScenario.launch(MainActivity::class.java)
+
+        scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+
+        onView(withId(R.id.profileImage)).check(doesNotExist())
+    }
+}
